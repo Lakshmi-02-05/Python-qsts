33ea40be253e90a7d1ac7da360295f62b6dcb42c
Lakshmi-02-05 <lak.csgirl@gmail.com>
2026-09-02 11:14:55 +0000

    Prime

--- Day2/Prime.py
+++ Day2/Prime.py
@@ -11,3 +11,13 @@ for i in range(2,51):
 
     if is_Prime:
         print(i,"is Prime number")
+
+# for i in range(1, 51):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print("FizzBuzz")
+#     elif i % 3 == 0:
+#         print("Fizz")
+#     elif i % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(i)
